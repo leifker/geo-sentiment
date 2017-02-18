e45ab9fd29c877a8569b9788e0da0bfe79ad66a5
leifker <dleifker@gmail.com>
2017-02-18 08:12:22 -0600

    fix tests

--- spark/src/integration-test/scala/com/github/leifker/spark/sentiment/AmazonReviewsIT.scala
+++ spark/src/integration-test/scala/com/github/leifker/spark/sentiment/AmazonReviewsIT.scala
@@ -1,7 +1,9 @@
 package com.github.leifker.spark.sentiment
 
+import com.datastax.spark.connector.CassandraRow
 import com.github.leifker.spark.AmazonReviewsTestContext
 import com.github.leifker.spark.test.ITest
+import org.apache.spark.rdd.RDD
 import org.scalatest.FlatSpec
 import org.scalatest.tagobjects.Slow
 
@@ -9,16 +11,15 @@ import org.scalatest.tagobjects.Slow
   * Created by dleifker on 2/16/17.
   */
 class AmazonReviewsIT extends FlatSpec {
-  val electronicsReviews = AmazonReviewsTestContext.amazonReviews.reviews.filter(_.getString("rootcategory") == "Electronics")
+  val electronicsReviews: RDD[CassandraRow] = AmazonReviewsTestContext.amazonReviews.reviews.filter(_.getString("rootcategory") == "Electronics")
     .sample(false, 0.001)
     .cache()
 
-  "Spark" should "be able to read reviews by category" taggedAs(ITest, Slow) in {
-    val rdd = electronicsReviews.take(1000)
-    assert(rdd.length == 1000)
+  "Spark" should "be able to process text reviews of sample rows" taggedAs(ITest, Slow) in {
+    electronicsReviews.foreach(row => NLPUtils.enchancedTokens(row.getString("reviewtext")))
   }
 
-  it should "be able get a large sample" taggedAs(ITest, Slow) in {
-    assert(electronicsReviews.sample(false, 0.001).count() >= 500)
+  it should "be able get at least a 500 sample" taggedAs(ITest, Slow) in {
+    assert(electronicsReviews.count() >= 500)
   }
 }
